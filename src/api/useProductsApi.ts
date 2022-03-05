1a2a88726f8549686bb639391bbde0f2c03e9861
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2022-03-05 16:15:52 +0100

    yeet console.log

--- src/api/useProductsApi.ts
+++ src/api/useProductsApi.ts
@@ -7,13 +7,11 @@ export const useProductsApi = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState<ProductApiDto>(null);
   const { state: filtersState } = useContext(FiltersContext);
-  console.log(filtersState);
 
   useEffect(() => {
     (async () => {
       setIsLoading(true);
       const queryString = stringify(filtersState);
-      console.log({ queryString, filtersState });
       const response = await makeRequestToProductApi(queryString);
       setData(response);
       setIsLoading(false);
